4bac6fc9a7b41a3531a06443c6a79b30af539765
thivenanderson <thanderson@g.hmc.edu>
2025-03-25 11:14:46 -0700

    yaw_des and yaw_error of surface control

--- main/SurfaceControl.cpp
+++ main/SurfaceControl.cpp
@@ -53,7 +53,8 @@ void SurfaceControl::navigate(xy_state_t * state, gps_state_t * gps_state_p, int
     // You can access the yaw calculated in XYStateEstimator.cpp using state->yaw
 
     ///////////////////////////////////////////////////////////
-    // INSERT P CONTROL CODE HERE
+   yaw_des = atan2(y_des - state->y, x_des - state->x)
+   yaw_error = angleDiff(yaw_des- state->yaw)
     ///////////////////////////////////////////////////////////
     
   }
