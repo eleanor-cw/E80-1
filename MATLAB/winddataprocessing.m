f937162b78e83bdf8d50926e40a4ed82ec40d039
thivenanderson <thanderson@g.hmc.edu>
2025-05-02 12:15:33 -0700

    wind data processing must use file 53

--- MATLAB/winddataprocessing.m
+++ MATLAB/winddataprocessing.m
@@ -1,16 +1,30 @@
 %file 53
-windcalibrate= u./10000;
-windcalibratesens=-(180/pi).*windcalibrate(457:550);
-windcalibrateimu=headingIMU(457:550);
+rotarysens=-u./10000*(180/pi);
+%Find true heading of the front of the bot
+offset = -30;  % IMU 0° = -30° true heading
+    % Step 1: Adjust for the IMU's misalignment
+    corrected_heading = headingIMU + offset;
+
+    % Step 2: Flip to get front-of-bot heading (since IMU is on back)
+    trueheadingRobot = corrected_heading + 180;
+
+    % Step 3: Wrap to [-180, 180]
+    trueheadingRobot = mod(trueheadingRobot + 180, 360) - 180;
+%Estimate wind as longest period of 0 degree rotatry output
+avgwind= mean(headingIMU(1236:1275))+180-30;
+avgwind= avgwind*ones(size(headingIMU));
 figure(1)
 clf
-plot(motorA)
+plot(trueheadingRobot)
 hold on
-plot(motorB)
-plot(motorC)
-plot(u./10000*(180/pi))
+plot(avgwind)
+plot(rotarysens)
 plot(headingIMU)
-legend('right motor','middle motor', 'left motor', 'rotary output','heading')
+legend('Adjusted robot heading', 'Estimated wind direction'...
+    , 'Rotary sensor output', 'IMU ouput')
+xlabel("Samples")
+ylabel("Degrees")
+title("Wind Correction Analysis (N=0 degrees)")
 xlim([457 1769])
 
 %note large imu spikes in  -30-> -180 degree range
